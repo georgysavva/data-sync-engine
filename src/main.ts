cd4b4d6277a0dfa3e7338235422167eb9ef237d2
Georgy Savva <georgy.savva@gmail.com>
2023-03-11 15:36:57 -0500

    tested out the rate limiter library

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import express from 'express';
 
+import { RateLimiterMemory } from 'rate-limiter-flexible';
+
 export function main(): void {
   const app = express();
   app.use(express.json());
 
-  app.post('/sync', (req, res) => {
+  const rateLimiter = new RateLimiterMemory({
+    points: 3, // 6 points
+    duration: 10, // Per second
+    execEvenly: true,
+  });
+
+  app.post('/sync', async (req, res) => {
     console.log(req.body);
+    for (let i = 0; i < 5; i++) {
+      console.log(i);
+      try {
+        const res = await rateLimiter.consume('foo', 1);
+        console.log('success 1', res);
+      } catch (error) {
+        console.log('error 1', error);
+      }
+    }
+
     res.json({ message: 'success' });
   });
 
@@ -14,4 +32,5 @@ export function main(): void {
     return console.log(`Express is listening at http://0.0.0.0:${port}`);
   });
 }
+
 main();
