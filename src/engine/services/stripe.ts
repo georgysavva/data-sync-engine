2eb8311819ca63e899ac241c496ee520048089a6
Georgy Savva <georgy.savva@gmail.com>
2023-03-13 08:56:41 -0400

    sync stripe customers

--- src/engine/services/stripe.ts
+++ src/engine/services/stripe.ts
@@ -13,13 +13,44 @@ export class StripeWorker {
     });
   }
   async sync(userAccount: UserAccount): Promise<void> {
+    console.log(
+      `Start syncing Stripe customers; userAccount=${userAccount.id}`,
+    );
     const stripeClient = new Stripe(userAccount.accessToken, {
       apiVersion: '2022-11-15',
     });
-    await this.apiRateLimiter.consume(userAccount.id, 1);
-    const customers = await stripeClient.customers.list({ limit: 100 });
-    console.log('customers', customers);
-    await this.db.stripeCustomer.findMany();
-    // return await this.prisma.task.findUniqueOrThrow({ where: { id } });
+
+    let startingAfter = undefined;
+    let hasMore = true;
+    let pageNumber = 1;
+    while (hasMore) {
+      await this.apiRateLimiter.consume(userAccount.id, 1);
+      console.log(
+        `Syncing Stripe customers page; userAccount=${userAccount.id}, page=${pageNumber}`,
+      );
+      const customersResult = await stripeClient.customers.list({
+        limit: 100,
+        starting_after: startingAfter,
+      });
+      const customers = customersResult.data.map((customerData) => {
+        return {
+          id: customerData.id,
+          email: customerData.email,
+          userAccountId: userAccount.id,
+          data: JSON.parse(JSON.stringify(customerData)),
+        };
+      });
+
+      await this.db.stripeCustomer.createMany({
+        data: customers,
+        skipDuplicates: true,
+      });
+      startingAfter = customers[customers.length - 1].id;
+      hasMore = customersResult.has_more;
+      pageNumber += 1;
+    }
+    console.log(
+      `Finished syncing Stripe customers; userAccount=${userAccount.id}`,
+    );
   }
 }
